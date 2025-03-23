ae10fbc9a714a5f5f9d94909a102804f908add5d
914-Laszlo-Marc <marclaszlolucian@gmail.com>
2025-03-23 18:24:45 +0200

    update

--- app.ts
+++ app.ts
@@ -1,12 +1,13 @@
-import cors from 'cors';
-import express from 'express';
-import expenseRoutes from './routes/expenseRoutes';
-import loginRouter from './routes/login-register';
+import cors from "cors";
+import express from "express";
+import expenseRoutes from "./routes/expenseRoutes";
+import loginRouter from "./routes/login-register";
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
-app.use('/api', expenseRoutes);
-app.use('/api', loginRouter);
+app.use("/api", expenseRoutes);
+app.use("/api", loginRouter);
 
 export default app;
